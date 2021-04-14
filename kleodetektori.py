f1f9207b17b3e233dd3ae49e3900dc68b5c8d22d
Rudy <52412061+Rudyyyyyyyyyyyy@users.noreply.github.com>
2021-04-14 15:02:27 +0400

    Add files via upload

---
+++ kleodetektori.py
@@ -0,0 +1,22 @@
+import time
+
+num=input("Ramdeni Procentit Ginda Rom Ikos Kle( Es Sxvas Ar Anaxo Ubralod Utiteb ): ")
+print("↓")
+print("↓")
+print("↓")
+print("↓")
+print("↓")
+print("↓")
+print("↓")
+print("↓")
+print("↓")
+print("↓")
+print("↓")
+print("↓")
+name = input("Klis Procentis Damtvleli. Chaweret Saxeli: ")
+print("Vitvli Klis Procents...")
+time.sleep(2)
+for i in range(int(num)):
+    print(name,i,"% It Klea")
+
+input("\n\nGavatave, Daachire ENTER Gasasvlelad.")
